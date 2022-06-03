9382be003c6c2ad68e78935abc8a71a6eee2a9e2
Geoffroy Bertrais <geoffroy.git@protonmail.com>
2022-06-03 18:25:05 +0200

    make NLP NGAP model predictions available

--- ephesus/nlp.py
+++ ephesus/nlp.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import string
+import joblib
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.model_selection import train_test_split
@@ -15,15 +16,30 @@ from ephesus.data import get_data_json, get_data_targets_json
 
 
 class TrainerNGAP():
-    def __init__(self, list_of_NGAP_codes = \
+    def __init__(self,
+                 list_of_NGAP_codes = \
                     ['PSG','PV19','TAID19','PSTA','PC19','TAIC19','SC',
                     'PVAG','IM','ABFM','PLVA','NUR1','PSTL','ADM2','CSC',
-                    'ADM1', 'OBSD', 'ISCI']
+                    'ADM1', 'OBSD', 'ISCI'],
+                 train_on_full_set = True,
+                 path_spacy="",
+                 path_ngap=""
                  ):
         '''
-        store list_of_NGAP_codes as a class attribute
+        Store list_of_NGAP_codes as a class attribute
+        If train_on_full_set == False then
+        - the train set is splitted in train, validation and test set
+        - model evaluation is enabled
+        - predictions are made on test set
+        If train_on_full_set == True then
+        - the whole train set is used for training and the model is saved
+        - model evaluation is impossible so it's disabled
+        - predictions load the model and make a single prediction
         '''
         self.list_of_NGAP_codes = list_of_NGAP_codes
+        self.train_on_full_set = train_on_full_set
+        self.path_spacy = path_spacy
+        self.path_ngap = path_ngap
         self.X = None # pandas DataFrame with one column named "X"
         self.y = None # pandas DataFrame with one column named "y"
         self.tokenizer = None
@@ -34,15 +50,15 @@ class TrainerNGAP():
         self.y_test = None
         self.ycols = None
 
-    def get_training_data(self, path):
+    def get_training_data(self):
         '''
-        path is the path to the spaCy model that extracts treatments from sentences
-        path is a string like "../models/model_v1/model-best"
+        path_spacy is the path to the spaCy model that extracts treatments from sentences
+        path_spacy is a string like "../models/model_v1/model-best"
         '''
 
         # load the training data for X
         # load the pre-trained spacy model
-        model = load_model(path)
+        model = load_model(self.path_spacy)
         # load the training data
         df = get_data_json()
         # run the model predictions on the data
@@ -72,6 +88,21 @@ class TrainerNGAP():
         self.X = df[["X"]]
         self.y = df[["y"]]
 
+    def remove_punctuation(self, text):
+        '''
+        preprocessing : remove punctuation
+        '''
+        for punctuation in string.punctuation:
+            text = text.replace(punctuation, ' ')
+        return text
+
+    def remove_stop_words(self, tokens):
+        '''
+        preprocessing : remove stop words
+        '''
+        stop_words = set(stopwords.words('french'))
+        return [token for token in tokens if token not in stop_words]
+
     def apply_tokenizer(self, X, maxlen=16):
         '''
         apply a previously fitted tokenizer
@@ -86,23 +117,15 @@ class TrainerNGAP():
 
     def train_ngap(self):
         '''
-        This is a draft of a function to train a model to recognise the NGAP code
+        train a model to recognise the NGAP code
         '''
-        # preprocessing
-        def remove_punctuation(text):
-            for punctuation in string.punctuation:
-                text = text.replace(punctuation, ' ')
-            return text
-        def remove_stop_words(tokens):
-            stop_words = set(stopwords.words('french'))
-            return [token for token in tokens if token not in stop_words]
         # preprocess X
         X = self.X.copy()
         X["X"] = X["X"].apply(str.lower)
         X["X"] = X["X"].apply(lambda x: ''.join(word for word in x if not word.isdigit()))
-        X["X"] = X["X"].apply(remove_punctuation)
+        X["X"] = X["X"].apply(self.remove_punctuation)
         X["X"] = X["X"].apply(word_tokenize)
-        X["X"] = X["X"].apply(remove_stop_words)
+        X["X"] = X["X"].apply(self.remove_stop_words)
         X = X["X"]
         # preprocess y
         y = self.y.copy()
@@ -114,8 +137,14 @@ class TrainerNGAP():
         y["_unknown_"] = (~ y["_unknown_"].astype(bool)).astype(int)
         y.drop(columns="y", inplace=True)
         self.ycols = y.columns
-        # train test split
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.33)
+
+        if self.train_on_full_set:
+            # no train test split if we want to train on the whole data set
+            self.X_train, self.X_test, self.y_train, self.y_test = X, None, y, None
+        else:
+            # train test split if we want to be able to evaluate our model
+            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.33)
+
         # create tokenizer and fit it on the train set
         self.tokenizer = Tokenizer()
         self.tokenizer.fit_on_texts(self.X_train)
@@ -140,43 +169,98 @@ class TrainerNGAP():
         es = EarlyStopping(patience=5, restore_best_weights=True)
         self.model.fit(X_train_pad, self.y_train, batch_size=8, epochs=50, validation_split=0.3, callbacks=[es])
 
+        if self.train_on_full_set:
+            # save the model to a joblib file
+            joblib.dump(self.model, self.path_ngap)
+
+
     def eval_ngap(self):
         '''
         evaluate the model
         '''
+
+        if self.train_on_full_set:
+            # cannot evaluate the model if we already used the whole data set for training
+            return -1
+
         # apply the tokenization on the test set and pad the test set
         X_test_pad = self.apply_tokenizer(self.X_test)
         # evaluate the model
         score = self.model.evaluate(X_test_pad, self.y_test)
         return score[1]
 
-    def predict_ngap(self):
+    def predict_ngap(self, sentence=""):
         '''
         use the model to make predictions
         '''
-        # apply the tokenization on the test set and pad the test set
-        X_test_pad = self.apply_tokenizer(self.X_test)
-        # use the trained model to make predictions
-        y_pred = self.model.predict(X_test_pad)
+        if self.train_on_full_set:
+            # load the model from the joblib file
+            try:
+                self.model = joblib.load(self.path_ngap)
+            except FileNotFoundError:
+                return pd.DataFrame()
+            # preprocessing
+            X = pd.DataFrame({"X" : [sentence]})
+            X["X"] = X["X"].apply(str.lower)
+            X["X"] = X["X"].apply(lambda x: ''.join(word for word in x if not word.isdigit()))
+            X["X"] = X["X"].apply(self.remove_punctuation)
+            X["X"] = X["X"].apply(word_tokenize)
+            X["X"] = X["X"].apply(self.remove_stop_words)
+            X = X["X"]
+            # apply the tokenization and padding
+            X_pad = self.apply_tokenizer(X)
+            # use the trained model to make predictions
+            y_pred = self.model.predict(X_pad)
+        else:
+            # apply the tokenization on the test set and pad the test set
+            X_test_pad = self.apply_tokenizer(self.X_test)
+            # use the trained model to make predictions
+            y_pred = self.model.predict(X_test_pad)
+
         # reshape y_pred to a more readable format
         df_pred = pd.DataFrame(y_pred)
         df_pred.columns = self.ycols
         df_pred = pd.DataFrame(df_pred.stack()).reset_index()
         df_pred = df_pred.sort_values(0).groupby("level_0").last()
-        df_pred_clean = pd.DataFrame(self.X_test).reset_index().drop(columns="index")
+
+        if self.train_on_full_set:
+            df_pred_clean = pd.DataFrame(X).reset_index().drop(columns="index")
+        else:
+            df_pred_clean = pd.DataFrame(self.X_test).reset_index().drop(columns="index")
+
         df_pred_clean["NGAP"] = df_pred["level_1"]
         df_pred_clean["softmax"] = df_pred[0]
         return df_pred_clean
 
 if __name__ == '__main__':
-    trainer = TrainerNGAP()
-    print("Load data:")
-    path = "../models/model_v2/model-best"
-    trainer.get_training_data(path)
-    print("Train:")
-    trainer.train_ngap()
-    print("Eval:")
-    score = trainer.eval_ngap()
-    print(f"model evaluation: {score}")
-    print("Predict:")
-    print(trainer.predict_ngap().head())
+
+    path_spacy = "../models/model_v2/model-best"
+    path_ngap = "../model_ngap.joblib"
+
+    test_evaluation = True
+    test_predict = True
+
+    if test_evaluation:
+        # create trainer for evaluation
+        print("Create trainer for evaluation:")
+        trainer = TrainerNGAP(train_on_full_set = False, path_spacy=path_spacy)
+        print("Load data:")
+        trainer.get_training_data()
+        print("Train:")
+        trainer.train_ngap()
+        print("Eval:")
+        score = trainer.eval_ngap()
+        print(f"model evaluation: {score}")
+        print("Predict:")
+        print(trainer.predict_ngap())
+    if test_predict:
+        # create trainer for precitions
+        print("Create trainer for predictions:")
+        trainer = TrainerNGAP(train_on_full_set = True, path_spacy=path_spacy, path_ngap=path_ngap)
+        print("Load data:")
+        trainer.get_training_data()
+        print("Train:")
+        trainer.train_ngap()
+        print("Predict:")
+        sentence = "Prise de sang réalisée ce jour au cabinet"
+        print(trainer.predict_ngap(sentence=sentence))
