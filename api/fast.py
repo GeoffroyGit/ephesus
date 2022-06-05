cf9b3a4142390e61ef8af97fa001e1c75ff4e777
Geoffroy Bertrais <geoffroy.git@protonmail.com>
2022-06-05 12:10:43 +0200

    complete api to expose predictions from treatment, date and time

--- api/fast.py
+++ api/fast.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 import joblib
 
+from ephesus.nlp import TrainerNGAP
+from ephesus.timedate import Date, Time
 
 app = FastAPI()
 
@@ -57,5 +59,54 @@ def test(sentence):
 def predict():
     return {"greeting": "not coded yet"}
 
+@app.get("/treatment")
+def treatment(sentence):
+    '''
+    return NGAP code corresponding to the treatment in sentence
+    '''
+    # initiate path to pre-trained models
+    path_spacy = "../models/model_v2/model-best"
+    path_ngap = "../model_ngap.joblib"
+    # create a new trainer for precitions
+    trainer = TrainerNGAP(train_on_full_set = True, path_spacy=path_spacy, path_ngap=path_ngap)
+    df = trainer.predict_ngap(sentence=sentence)
+    result = {
+        "NGAP" : df["NGAP"][0],
+        "softmax" : df["softmax"][0]
+    }
+    return result
+
+@app.get("/date")
+def date(sentence):
+    '''
+    return date in date format
+    '''
+    test = Date()
+    df = test.transform_data(sentence=sentence)
+    result = {
+        "day" : df["day"][0],
+        "month" : df["month"][0],
+        "year" : df["year"][0],
+        "day_of_week" : df["day_of_week"][0],
+        "day_from_today" : df["day_from_today"][0]
+    }
+    return result
+
+@app.get("/time")
+def time(sentence):
+    '''
+    return time in time format
+    '''
+    test = Time()
+    df = test.transform_data(sentence=sentence)
+    result = {
+        "hour" : df["time"][0][0],
+        "minute" : df["time"][0][1]
+    }
+    return result
+
 if __name__ == "__main__":
     print(test("hello"))
+    print(treatment("Prise de sang"))
+    print(date("2 septembre 2022"))
+    print(time("18h45"))
